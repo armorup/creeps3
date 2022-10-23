29e39b9c3c26509de134a4b4b8fe807669c2a9eb
Stewart Chan <stew.chan@gmail.com>
2022-10-23 16:09:12 -0400

    Add score

--- Main.gd
+++ Main.gd
@@ -22,6 +22,7 @@ func _on_MobTimer_timeout():
 		var player_position = $Player.transform.origin
 		mob.initialize(mob_spawn_location.translation, player_position)
 		add_child(mob)
+		mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
 
 
 func _on_Player_hit():
